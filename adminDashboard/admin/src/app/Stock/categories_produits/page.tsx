f837ca818f5f317205692eaed7ad44b7f31ec324
premice <136833613+djoli-itproject@users.noreply.github.com>
2025-03-07 13:27:45 +0000

    add structure of project

---
+++ adminDashboard/admin/src/app/Stock/categories_produits/page.tsx
@@ -0,0 +1,88 @@
+import Datatable from "@/components/tables/dataTable";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+type Categories = {
+
+  id: number;
+
+  nom: string;
+
+  description: string | null;
+
+  parentId: number | null;
+
+ 
+  parent: Categories;
+
+  categories: Categories[];
+
+}
+export default function Categorie(){
+
+  const { data: session, status } = useSession(); // Récupérer la session utilisateur
+  const [datalist, setdatalist] = useState<Categories[]>([]);
+
+  useEffect(() => {
+      const fetchJournaux = async () => {
+          if (status !== "authenticated") return; // Ne fait rien si l'utilisateur n'est pas connecté
+
+          console.log("🔄 Chargement des journaux...",session?.user?.accessToken);
+          try{
+            const response = await fetch("http://localhost:3003/journal-services", {  // Ton API NestJS
+              method: "GET",
+              headers: { 
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${session?.user?.accessToken}`
+               },
+            });
+            if (!response.ok) {
+              throw new Error(`Erreur HTTP : ${response.status} ${response.statusText}`);
+            }
+            // drgodrogo
+            const data: Categories[]  = await response.json();
+            console.log("📜 Journaux reçus :", data);
+            // Vérifie que les données sont un tableau
+            if (!Array.isArray(data )) {
+              throw new Error("Les données reçues ne sont pas un tableau de journaux.");
+            }
+            setdatalist(data);    
+          }catch(error){
+            console.error("❌ Erreur lors du chargement des journaux :", error);
+          }
+      };
+      fetchJournaux();
+  }, [session, status]);
+
+  return (
+    <>
+         <div className="content-wrapper">
+        <section className="content-header">
+          <h1>
+            Logs
+            <small>Journal Service</small>
+          </h1>
+          <ol className="breadcrumb">
+            <li><a href="#"><i className="fa fa-dashboard"></i> Logs</a></li>
+            <li><a href="#">Journal</a></li>
+            <li className="active"> Listes</li>
+          </ol>
+        </section>
+
+        <section className="content">
+            <div className="row">
+                <div className="col-xs-12">
+                    <div className="box box-primary">
+                        <div className="box-body">
+                        <Datatable tableau={datalist}/> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    </>
+  )
+}
+
+  
